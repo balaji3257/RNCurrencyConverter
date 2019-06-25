2aa0a09dc2ad17a0b94ffdf6b63bed04d6d08173
Balaji Sreenivasan <balaji.sreenivasan@imaginea.com>
2019-06-25 11:22:16 +0530

    updated ui and added picker for currency selection

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  Alert,FlatList
+  Alert,FlatList, Picker
 } from 'react-native';
+
+import { Swiper } from 'react-native-swiper';
 
 export default class App extends React.Component{
   
@@ -17,7 +19,7 @@ export default class App extends React.Component{
       conversionResult: '0.00',
       inrValue: 0,
       isLoading: true,
-      globalCurrencyRate:''
+      globalCurrencyRate:null
     }
     this.convButtonPressed = this.convButtonPressed.bind(this);
   }
@@ -25,6 +27,9 @@ export default class App extends React.Component{
     this.getRealTimeCurrencyConversionRates();
   }
 
+  /**
+   * @description : realtime currency conversion rates are fetch from 'api.exchangeratesapi.io'
+   */
   getRealTimeCurrencyConversionRates = async ()=>{
     return fetch('https://api.exchangeratesapi.io/latest?base=INR')
     .then((response) => response.json())
@@ -37,6 +42,9 @@ export default class App extends React.Component{
     })
   }
 
+  /**
+   * @description: simple method shows the implemetation of the 'fetch api' working
+   */
   getMoviesList = async () =>{
     return  fetch('https://facebook.github.io/react-native/movies.json')
     .then((response) => response.json())
@@ -50,6 +58,9 @@ export default class App extends React.Component{
     });
   }
 
+  /**
+   * @description: Convert the currency values for the given currencies
+   */
   convButtonPressed = (currencyType) => {
     var currencyRate = 0.00
     if((this.state.inrValue===0) || (isNaN(this.state.inrValue)) ){
@@ -70,120 +81,135 @@ export default class App extends React.Component{
     }
       
   }
-
+  /**
+   * @description: used to handle the input value from user input field
+   */
   handleChangeInput = (text) => {
     this.setState({ inrValue: text })
   }
 
+  /**
+   * @description : Calling when rotate currency button is pressed and 
+   *                  update the from and to currency values.
+   */
+  rotateButtonPressed = () => {
+
+  }
   render(){
     return(
-      <View style={styles.container}>      
-        <View style={styles.resultViewer}>
-          <Text style={styles.outputText}>{this.state.conversionResult}</Text>
-        </View>
-        <View style={
-            { justifyContent: 'center', alignItems:'center', flexDirection: 'row',
-            backgroundColor:'#b2dfdb', marginTop: 20, borderRadius: 12
-            }}>
-          <Image style={
-                {
-                  width: 25,height: 25,marginLeft: 10
-                }} 
+      <View style={styles.container}> 
+
+        <View style={ styles.inputContainer }>
+          <Image style={ styles.inputCurrencyIcon } 
                 source={require('./src/assets/icons/indian.png')}/>
-                <View
-                  style={{
-                    backgroundColor:"gray", width:2, height:'80%', marginLeft:10
-                  }}
-                  >
-                </View>
-          
-            <TextInput style={
-              {   
-                flex: 1, color:'black',
-                fontSize: 20, fontWeight: 'bold', paddingLeft: 20,
-                height: 50
-              }
-            }
-              placeholder="Enter amount"
-              placeholderTextColor="black"
-              keyboardType="numeric"
-              onChangeText={this.handleChangeInput}
-              />
+          <TextInput style={styles.inputText}
+            placeholder="0" placeholderTextColor="black" keyboardType="numeric"
+            onChangeText={this.handleChangeInput} />
         </View>
-        <View style={{   
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          marginVertical: 30,
-          marginHorizontal: -5,
-          justifyContent: 'space-between',
-          paddingTop: 40
-        }}>
-              <TouchableOpacity onPress={()=>this.convButtonPressed('USD')} style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>USD</Text>
+          <View style={styles.pickerContainer}>
+            <Text style={styles.fromText}>From:</Text>
+            <Picker selectedValue={this.state.lan}
+                    style={styles.languagePicker} itemStyle={styles.lanItem}
+                    onValueChange={(itemValue, itemIndex) =>{this.setState({lan: itemValue})}}>
+                <Picker.Item label="USD" value="usDollar" />
+                <Picker.Item label="EUR" value="Euro" />
+                <Picker.Item label="INR" value="IndianRupee" />
+                <Picker.Item label="AUD" value="AusDollar" />    
+            </Picker>
+          </View>
+
+          <View>
+            <TouchableOpacity onPress={() =>{this.rotateButtonPressed}}>
+              <Image source={require('./src/assets/icons/convert.png')}
+                    style={styles.currRotateButton}/>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.pickerContainer}>
+            <Text style={styles.fromText}>To:</Text>
+            <Picker selectedValue={this.state.lan}
+                    style={styles.languagePicker} itemStyle={styles.lanItem}
+                    onValueChange={(itemValue, itemIndex) =>{this.setState({lan: itemValue})}}>
+                <Picker.Item label="USD" value="usDollar" />
+                <Picker.Item label="EUR" value="Euro" />
+                <Picker.Item label="INR" value="IndianRupee" />
+                <Picker.Item label="AUD" value="AusDollar" />      
+            </Picker>
+          </View>
+        <View style={ styles.convertButtonContainer }>
+              <TouchableOpacity style={styles.buttonContainer}
+                      onPress={()=>this.convButtonPressed('USD')} >
+                <Text style={styles.buttonText}>CONVERT</Text>
               </TouchableOpacity >
-              <TouchableOpacity onPress={()=>this.convButtonPressed('EUR')} style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>EUR</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={()=>this.convButtonPressed('AUD')} style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>AUD</Text>
-              </TouchableOpacity>
         </View>
+        <View style={styles.resultContainer}>
+            <Text style={styles.resultText}>2344.99</Text>
+            <Text style={styles.resultCurrenyText}>USD</Text>
+        </View>
+        
   </View>
     );
   }
-
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#487EB0',
-    alignItems: 'center',
+    flex: 1, backgroundColor: '#758AA2', alignItems: 'center', padding: 20
+  },
+  inputContainer:{ 
+    flexDirection: 'row', backgroundColor:'#fff', marginTop: 20, borderRadius: 12,
+    marginVertical: 40, height: 120,justifyContent: 'center', alignItems:'center'
+  },
+  inputCurrencyIcon:{
+    width: 25,height: 25, padding:22, marginLeft: 10, alignSelf: 'center'
+  },
+  inputText:{   
+      flex: 1, color:'black', fontSize: 60, fontWeight: 'bold', paddingRight: 20,textAlign:'right'
+  },
+  
+  buttonText: {
+    fontSize: 24, fontWeight: 'bold', textAlign: 'center', textAlignVertical: 'center',
+  },
+  currSelectionContainer:{
+    width: '100%',
+    borderColor: 'black',
+    borderRadius: 20,
+    borderWidth:2,
     padding: 20
   },
-  resultViewer: {
-    height: 80,
-    width: '100%',
-    justifyContent: "center",
-    backgroundColor: '#fff',
-    alignItems: "center",
-    borderRadius: 20,
-    marginTop: 60,
+  languagePicker:{
+    height: 60, borderWidth: 4, borderColor: 'red', borderRadius: 25, flex: 3, marginLeft: 20
   },
-  outputText: {
-    fontSize: 25,
-    fontWeight: 'bold',
+  lanItem:{
+    fontWeight: 'bold', alignItems:'center'
   },
-  inputViewer: {
-    height: 80,
-    width: '95%',
-    justifyContent: "center",
-    backgroundColor: '#fff',
-    alignItems: "center",
-    borderRadius: 20,
-    marginTop: 30,
+  pickerContainer:{
+    alignItems: 'center', justifyContent:'center', flexDirection: 'row',backgroundColor: '#e0f7fa',
+    height: 'auto',
   },
-  inputText: {
-    height: 60,
-    width: '100%',
-    fontWeight: '600',
-    fontSize: 23
+  fromText:{
+    flex:1, fontSize:30, fontWeight:'600', textAlign:'center', textAlignVertical:'center'
   },
-  buttonText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    textAlignVertical: 'center',
+  currRotateButton:{
+    height: 50, width: 50, margin: 20
+  },
+  convertButtonContainer:{   
+   width: '100%', marginTop: 30
   },
   buttonContainer: {
-    width: '28.33%',
-    height: 50,
-    borderWidth: 2,
-    backgroundColor: '#67E6DC',
-    borderRadius: 12,
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 5
+    height: 50, borderWidth: 1, backgroundColor: '#26ae60', borderRadius: 12,
+    justifyContent: "center", alignItems: "center",
+  },
+  resultContainer:{
+      backgroundColor: 'white', width:'100%', height: 120, justifyContent: 'center', alignItems:'center',
+      marginTop: 25, borderRadius: 12
+  },
+  resultText:{
+    fontSize: 35,
+    fontWeight:'800'
+  },
+  resultCurrenyText:{
+    fontWeight: '600', fontSize: 20, color: '#EA7773'
   }
+  
 });
